f8bb6e6b6f0e950cc2cb8ce3fbc084ec40de5103
schr2001smh <schr2001@gmail.com>
2024-12-03 03:35:29 -0800

    finished addsettlement+addplan
    addsettlement+addplan got everything except the act function

--- Skeleton/src/Action.cpp
+++ Skeleton/src/Action.cpp
@@ -20,20 +20,70 @@ void BaseAction::error(string errorMsg){
 }
 
 const string &BaseAction::getErrorMsg() const{return errorMsg;}
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////Add Plan class
 
-//Add Plan class
-
-//constructor
+///constructor
 AddPlan::AddPlan(const string &settlementName, const string &selectionPolicy)
 :settlementName(settlementName), selectionPolicy(selectionPolicy){}
 
+/////////////////////////////////// selection policy/settlement name inccorect need to return an error
 void AddPlan::act(Simulation &simulation){
+    if (!simulation.isSettlementExists(settlementName))
+    {
+       error("Cannot create this plan");
+    }
+    
     Settlement *settlementPtr = &simulation.getSettlement(settlementName);
     if(selectionPolicy == "nve"){
-        simulation.addPlan(*settlementPtr, &NaiveSelection());
+        simulation.addPlan(simulation.getSettlement(settlementName), &NaiveSelection());
+
     }else if(selectionPolicy == "bal"){
-         simulation.addPlan(*settlementPtr, &BalancedSelection(settlementPtr) )
+        simulation.addPlan(simulation.getSettlement(settlementName), &BalancedSelection(0,0,0) );
+
+    }else if (selectionPolicy == "eco")
+    {
+        simulation.addPlan(simulation.getSettlement(settlementName), &EconomySelection());
+
+    }else if (selectionPolicy == "env")
+    {
+        simulation.addPlan(simulation.getSettlement(settlementName), &SustainabilitySelection());
+
     }
-    }
-    simulation.addPlan(simulation.getSettlement(settlementName), selectionPolicy);
+    else  error("Cannot create this plan");
+    
 }
+///////////////////// not sure of the syntax but something like this
+ const string AddPlan::toString() const{
+    return "settlement= "+ settlementName +" selection policiy= "+selectionPolicy;
+ }
+//////////////////////// this should be enough?
+ AddPlan* AddPlan::clone() const{
+return new AddPlan(settlementName,selectionPolicy);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////Add Settlement class
+AddSettlement::AddSettlement(const string &settlementName,SettlementType settlementType):
+settlementName(settlementName),
+settlementType(settlementType)
+{}
+
+AddSettlement*AddSettlement::clone() const{
+    return new AddSettlement(settlementName,settlementType);
+}
+
+ const string AddSettlement::toString() const{
+    switch (settlementType)
+    {
+    case SettlementType::CITY:
+         return "settlementName= "+ settlementName +" settlement type= " + "CITY" ;
+          break;
+    case SettlementType::VILLAGE:
+        return "settlementName= "+ settlementName +" settlement type= " + "VILLAGE" ;
+        break;
+    case SettlementType::METROPOLIS:
+        return "settlementName= "+ settlementName +" settlement type= " + "METROPOLIS" ;
+        break;
+    }
+ }
